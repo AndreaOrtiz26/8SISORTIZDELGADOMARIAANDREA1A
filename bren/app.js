3620eb17600e6483d88586ec9a8b2565eab5cd37
AndreaOrtiz26 <ma.ortiz.d1@edu.utc.mx>
2023-02-06 21:14:50 -0600

    usuario

---
+++ bren/app.js
@@ -0,0 +1,12 @@
+function ir(){
+
+    var n=040722;
+    var c="Brenda";
+
+    if(document.form.password.value==n && document.form.login.value==c){
+        alert("Bienvenido");}
+        else{
+            alert("Los datos son incorrectos, por favor intente de nuevo");
+        }
+
+        }
